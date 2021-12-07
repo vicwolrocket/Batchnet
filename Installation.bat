695b056e52dd253b3380d6a1ae3b4f086f013dc3
Victor <vixstixboom@gmail.com>
2021-12-07 09:06:26 +0200

    Still working on installer...

--- Installation.bat
+++ Installation.bat
@@ -1,7 +1,10 @@
 @echo off
+title Installing...
 cd C:\
 if not exist batchnet md batchnet
 cd batchnet
-if not exist ift.rar powershell -Command "Invoke-WebRequest https://github.com/vicwolrocket/Batchnet/blob/main/if.rar?raw=true -Outfile ift.rar"
-md mainfiles
-powershell -Command "Expand-Archive ift.rar"
+del /q files.zip
+powershell -Command "Invoke-WebRequest https://github.com/vicwolrocket/Batchnet/blob/main/if.zip?raw=true -Outfile files.zip"
+rmdir /s /q files
+copy Batchnet.exe c:\Users\%username%\Desktop\Batchnet.exe
+powershell -Command "Expand-Archive files.zip"
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\batchnet\files
+main.bat
