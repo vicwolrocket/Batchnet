2e9236436930726ccdb413589873be462ccdc4f4
Victor <vixstixboom@gmail.com>
2021-12-07 08:53:27 +0200

    Change Installation files

--- Installation.bat
+++ Installation.bat
@@ -1,2 +1,7 @@
 @echo off
-powershell -Command "Invoke-WebRequest
+cd C:\
+if not exist batchnet md batchnet
+cd batchnet
+if not exist ift.rar powershell -Command "Invoke-WebRequest https://github.com/vicwolrocket/Batchnet/blob/main/if.rar?raw=true -Outfile ift.rar"
+md mainfiles
+powershell -Command "Expand-Archive ift.rar"
